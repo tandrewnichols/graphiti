ce4aa8286b54b0f6a1939ead66fb862284d856da
Andrew Nichols <tandrewnichols@gmail.com>
2017-09-17 10:04:57 -0400

    Finish graph test

--- test/unit/graph.js
+++ test/unit/graph.js
@@ -7,17 +7,24 @@ const neo4j = {
     driver: sinon.stub()
   }
 }
+const globby = sinon.stub()
 const cleanup = sinon.stub()
+class Foo {}
+class Bar {}
 const Graph = require('proxyquire').noCallThru()('../../lib/graph', {
   'neo4j-driver': neo4j,
-  'node-cleanup': cleanup
+  'node-cleanup': cleanup,
+  globby: globby,
+  foo: Foo,
+  bar: Bar
 })
 
 describe('Graph', () => {
   let graph
   let driver = {
     onCompleted: sinon.stub(),
-    onError: sinon.stub()
+    onError: sinon.stub(),
+    close: sinon.stub()
   }
 
   afterEach(() => {
@@ -96,6 +103,120 @@ describe('Graph', () => {
         graph.error.should.be.true()
         graph.emit.should.have.been.calledWith('error')
       })
+    })
+  })
+
+  describe('#onComplete', () => {
+    beforeEach(() => {
+      graph = new Graph()
+      graph.onComplete('fn')
+    })
+
+    it('should call driver.onCompleted', () => {
+      graph.driver.onCompleted.should.have.been.calledWith('fn')
+    })
+  })
+
+  describe('#onError', () => {
+    beforeEach(() => {
+      graph = new Graph()
+      graph.onError('fn')
+    })
+
+    it('should call driver.onError', () => {
+      graph.driver.onError.should.have.been.calledWith('fn')
+    })
+  })
+
+  describe('#register', () => {
+    let cb = sinon.stub()
+    beforeEach(() => {
+      sinon.stub(Graph.prototype, 'registerNodeModels')
+      sinon.stub(Graph.prototype, 'registerRelationshipTypes')
+      Graph.prototype.registerNodeModels.withArgs('nodes').resolves('foo')
+      Graph.prototype.registerRelationshipTypes.withArgs('relationships').resolves('bar')
+      graph = new Graph()
+      return graph.register({ nodes: 'nodes', relationships: 'relationships' }).then(cb)
+    })
+
+    afterEach(() => {
+      Graph.prototype.registerNodeModels.restore()
+      Graph.prototype.registerRelationshipTypes.restore()
+    })
+
+    it('should register node models and then relationship types', () => {
+      cb.should.have.been.calledWith({
+        nodeModels: 'foo',
+        relationshipTypes: 'bar'
+      })
+    })
+  })
+
+  describe('#registerNodeModels', () => {
+    let cb = sinon.stub()
+
+    beforeEach(() => {
+      sinon.stub(Graph.prototype, '_register')
+      Graph.prototype._register.withArgs('patterns').resolves([Foo, Bar])
+      graph = new Graph()
+      return graph.registerNodeModels('patterns').then(cb)
+    })
+
+    afterEach(() => {
+      Graph.prototype._register.restore()
+    })
+
+    it('should assign the models to the graph instance', () => {
+      graph.nodeModels.should.eql([Foo, Bar])
+      graph.Foo.should.eql(Foo)
+      graph.Bar.should.eql(Bar)
+      cb.should.have.been.calledWith([Foo, Bar])
+    })
+  })
+
+  describe('#registerRelationshipTypes', () => {
+    let cb = sinon.stub()
+
+    beforeEach(() => {
+      sinon.stub(Graph.prototype, '_register')
+      Graph.prototype._register.withArgs('patterns').resolves([Foo, Bar])
+      graph = new Graph()
+      return graph.registerRelationshipTypes('patterns').then(cb)
+    })
+
+    afterEach(() => {
+      Graph.prototype._register.restore()
+    })
+
+    it('should assign the types to the graph instance', () => {
+      graph.relationshipTypes.should.eql([Foo, Bar])
+      graph.Foo.should.eql(Foo)
+      graph.Bar.should.eql(Bar)
+      cb.should.have.been.calledWith([Foo, Bar])
+    })
+  })
+
+  describe('#_register', () => {
+    let cb = sinon.stub()
+
+    beforeEach(() => {
+      globby.withArgs('patterns').resolves(['foo', 'bar'])
+      return graph._register('patterns').then(cb)
+    })
+
+    it('should require all models', () => {
+      cb.should.have.been.calledWith([Foo, Bar])
+    })
+  })
+
+  describe('#close', () => {
+    beforeEach(() => {
+      graph = new Graph()
+      graph.close()
+    })
+
+    it('should call driver.close', () => {
+      driver.close.should.have.been.called
     })
   })
 })
